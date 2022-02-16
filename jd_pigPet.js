d59df33fa716070969e56f1aaf2f1ad3b0cb272a
ougapia <ougapia@hotmail.com>
2022-02-16 14:41:39 +0800

    Update jd_pigPet.js

--- jd_pigPet.js
+++ jd_pigPet.js
@@ -25,7 +25,7 @@ cron "12 0-23/6 * * *" script-path=jd_pigPet.js, tag=京东金融养猪猪
  */
 
 const $ = new Env('金融养猪');
-const url = require('url');
+const url = 'https://u.jr.jd.com/uc-fe-wxgrowing/cloudpig/index/';
 let cookiesArr = [], cookie = '', allMessage = '';
 const JD_API_HOST = 'https://ms.jr.jd.com/gw/generic/uc/h5/m';
 const MISSION_BASE_API = `https://ms.jr.jd.com/gw/generic/mission/h5/m`;
